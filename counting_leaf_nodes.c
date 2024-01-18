b2d853cc87d1f88b14202d8acbb0ac050cd0177e
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-01-18 18:47:02 +0530

    Counting leaf node in a binary tree

--- counting_leaf_nodes.c
+++ counting_leaf_nodes.c
@@ -1,15 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node 
+struct node
 {
     int data;
     struct node *left;
     struct node *right;
 };
 
-struct node *newNode(int data){
-    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+struct node *createNode(int data)
+{
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
     newNode->data = data;
     newNode->left = NULL;
     newNode->right = NULL;
@@ -17,27 +18,32 @@ struct node *newNode(int data){
     return newNode;
 }
 
-int getLeafCount(struct node *node){
-    if(node == NULL)
-      return 0;
+int getLeafCount(struct node *node)
+{
+    if (node == NULL)
+        return 0;
 
-    if(node->left == NULL && node->right == NULL){
+    if (node->left == NULL && node->right == NULL)
+    {
         return 1;
     }
-    else{
+    else
+    {
         return getLeafCount(node->left) + getLeafCount(node->right);
     }
-
 }
 
-int main(){
-    struct node *root = newNode(1);
-    root->left = newNode(2);
-    root->right = newNode(3);
-    root->left->left = newNode(4);
-    root->left->right = newNode(5);
-    root->right->left = newNode(6);
-    root->right->right = newNode(7);
+int main()
+{
+    struct node *root = createNode(4);
+    struct node *n1 = createNode(1);
+    struct node *n2 = createNode(6);
+    struct node *n3 = createNode(5);
+    struct node *n4 = createNode(2);
+    root->left = n1;
+    root->right = n2;
+    n1->left = n3;
+    n1->right = n4;
 
     printf("Leaf count of the tree is %d", getLeafCount(root));
     return 0;
